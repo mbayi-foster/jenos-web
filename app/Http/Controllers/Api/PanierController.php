128acca288f3ca193a0aa61f59187a77b92a0764
Foster <mbayifoster@gmail.com>
2025-03-26 13:58:49 +0100

    amelioration pour les photos

--- app/Http/Controllers/Api/PanierController.php
+++ app/Http/Controllers/Api/PanierController.php
@@ -5,9 +5,18 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use App\Models\Panier;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
 
 class PanierController extends Controller
 {
+    private $url;
+    private $photo = "https://cdn.pixabay.com/photo/2024/09/15/13/03/cows-9049119_1280.jpg";
+
+
+    public function __construct()
+    {
+        $this->url = env("APP_URL");
+    }
     /**
      * Display a listing of the resource.
      */
@@ -35,7 +44,7 @@ class PanierController extends Controller
             $panier->qte += $validated["qte"];
             $panier->save();
             return response()->json($panier, 201);
-        } else if(!$panier) {
+        } else if (!$panier) {
             $panier = Panier::create(
                 [
                     "prix" => $validated["prix"],
@@ -58,13 +67,34 @@ class PanierController extends Controller
     public function show(string $id)
     {
         $panier_before = Panier::where("client_id", $id)->with("plats")->get();
-
+        $paniers = [];
         foreach ($panier_before as $panier) {
-            $panier[]= [
-                
+            $plat = $panier->plats;
+            $url = Storage::disk('public')->url($plat->photo);
+            if (strpos($url, 'http://localhost') !== false) {
+                $url = str_replace('http://localhost', 'http://localhost:8000', $url); // Remplacez par le port approprié
+            }
+            $paniers[] = [
+                "id"=>$panier->id,
+                "client_id"=>$panier->id,
+                "status"=>$panier->status,
+                "prix"=>$panier->prix,
+                "qte"=>$panier->qte,
+                "created_at"=>$panier->created_at,
+                "plat" => [
+                    "id" => $plat->id,
+                    'nom' => $plat->nom,
+                    'details' => $plat->details,
+                    "like" => $plat->like,
+                    'photo' => $this->photo, //$url,
+                    'qte' => $plat->qte,
+                    'prix' => $plat->prix,
+                    'created_at' => $plat->created_at
+                ],
+
             ];
         }
-        return response()->json($panier_before, 200);
+        return response()->json($paniers, 200);
     }
 
     /**
@@ -78,14 +108,14 @@ class PanierController extends Controller
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(String $id)
+    public function destroy(string $id)
     {
         $panier = Panier::where("id", $id)->first();
 
-        if($panier){
+        if ($panier) {
             $panier->delete();
             return response()->json(true, 200);
-        }else{
+        } else {
             return response()->json(false, 500);
         }
     }
