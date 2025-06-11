326af4430fee1546dcb953ae21996df3111d8dfc
Foster <mbayifoster@gmail.com>
2025-06-11 15:04:39 +0100

    fix error

--- app/Http/Controllers/Api/CommandeController.php
+++ app/Http/Controllers/Api/CommandeController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use App\Models\Client;
 use App\Models\Commande;
+use App\Models\Livreur;
 use App\Models\Commune;
 use App\Models\Notification;
 use App\Models\Panier;
